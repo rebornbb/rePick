21e8050fb55a1d3f4e9e3f6fe309f94a656e50a4
rebornbb <bongnarye@gmail.com>
2022-10-11 01:26:46 +0900

    index페이지 작업 1차완료

--- src/main/java/com/gosari/repick_project/news/NewsController.java
+++ src/main/java/com/gosari/repick_project/news/NewsController.java
@@ -3,6 +3,7 @@ package com.gosari.repick_project.news;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -23,4 +24,5 @@ public class NewsController {
 
         return "news";
     }
+
 }
--- src/main/java/com/gosari/repick_project/question/QuestionForm.java
+++ src/main/java/com/gosari/repick_project/question/QuestionForm.java
@@ -17,7 +17,7 @@ public class QuestionForm {
 
     @NotEmpty(message = "내용은 필수항목입니다.")
     private String content;
-    
+
     /*카테고리메세지*/
     @NotBlank(message = "카테고리선택은 필수항목입니다.")
     private String category;
